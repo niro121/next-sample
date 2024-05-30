e4c02dd37eceddd7fd2e9f4488b1e246d5a67e81
Nirosha Kodituwakku <nirosha@archmage.lk>
2024-05-30 16:07:11 +0530

    niro

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.scss";
+import "../../styles/backend.scss";
+
 import Backendheader from "../../components/backendheader";
 
 const inter = Inter({ subsets: ["latin"] });
